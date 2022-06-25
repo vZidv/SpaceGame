427885e1117af8dc706c91b2388b376be50541a9
vZidv <94315999+vZidv@users.noreply.github.com>
2022-06-25 20:53:59 +0300

    ver 0.92

---
+++ Strong kitty/Assets/Scripts/Menu_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour
+{
+    public GameObject blackScrean;
+    private Animator blackScreanAni;
+    void Start()
+    {
+        blackScrean = GameObject.Find("BlackScrean");
+        blackScrean.SetActive(false);      
+        blackScreanAni = blackScrean.GetComponent<Animator>();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Play()
+    {
+        StartCoroutine(PlayStart());
+    }
+    IEnumerator PlayStart()
+    {
+        blackScrean.SetActive(true);
+        blackScreanAni.SetInteger("Status", 2);
+        yield return new WaitForSeconds(1.4f);
+        blackScreanAni.SetInteger("Status", 0);
+        yield return new WaitForSeconds(1.4f);
+
+        SceneManager.LoadScene("GameScene");
+        yield return new WaitForSeconds(1.4f);
+        blackScrean.SetActive(false);
+    }
+}
